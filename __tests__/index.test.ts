98050ce0ec93d49a7e602277186675f2e4bf4a09
Fuxuan Gong <fuxuan.gong@nextgentel.com>
2021-07-27 15:41:10 +0200

    initial commit

---
+++ __tests__/index.test.ts
@@ -0,0 +1,55 @@
+import TimeBuffered from "../src/index";
+import "jest-extended";
+
+test("test TimeBuffered with default options", () => {
+  //mock Date.now() function, which is used in the code to check if state is expired.
+  let now = 1530518207007;
+  global.Date.now = jest.fn(()=>now);
+  expect(Date.now() ).toBe(now);
+
+  //make sure the timer mock works, the default "modern" doesn't work for me
+  jest.useFakeTimers("legacy");
+  setTimeout(() => {}, 0);
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+  jest.clearAllMocks();
+
+  let loadingState = new TimeBuffered();
+  // test first push
+  loadingState.push("loading", 1000);
+  expect(loadingState.size()).toEqual(1);
+  expect(loadingState.get()[0]).toEqual("loading");
+  expect(setTimeout).toHaveBeenCalledTimes(0);
+
+  // the state stays if there is no new push, after 2s which > minDuration.
+  now += 2000;
+  expect(loadingState.size()).toEqual(1);
+  expect(loadingState.get()[0]).toEqual("loading");
+  expect(setTimeout).toHaveBeenCalledTimes(0);
+
+  // push the second value after minDuration passed: 
+  //     the stage will be transite immediately
+  loadingState.push("failed", 1000);
+  expect(setTimeout).toHaveBeenCalledTimes(0);
+  expect(loadingState.size()).toEqual(1);
+
+  now += 500;
+  expect(loadingState.size()).toEqual(1);
+  expect(loadingState.get()[0]).toEqual("failed");
+
+  // push the 3rd value before 0.5s which < minDuration 
+  loadingState.push("loading", 1000);
+  expect(loadingState.size()).toEqual(2);
+  expect(loadingState.get()[0]).toEqual("loading");
+  expect(loadingState.get()[1]).toEqual("failed");
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+  expect(setTimeout).toHaveBeenLastCalledWith(
+    expect.any(Function),
+    expect.toBeWithin(400, 600)
+  );
+
+   // test empty push
+  now += 2000;
+  expect(loadingState.get()[0]).toEqual("loading");
+  loadingState.push(); //push nothing after 2s > minDuration, it pushes away the existing values 
+  expect(loadingState.size()).toEqual(0);
+});
---
+++ src/index.ts
@@ -0,0 +1,148 @@
+/**
+ * Do your messages change too fast for users to read? The TimeBuffered here is to rescue.
+ * Define a min duration time in the message, so the next message will not push it away immedialely,
+ * but get queued behind it until the min-duration time passed.
+ */
+
+/**
+ * The internal storage item in the buffer
+ * @property value the new value pushed to the buffer
+ * @property minDuration the minimum duration time given in milliseconds. If there is a new value pushed, the current value will stay until the min-duration time passed if not yet.
+ *
+ * @property timestamp the timestamp in millisconds, which recorded when the value gets pushed
+ */
+export interface TimeBufferedItem<T> {
+  value: T;
+  minDuration: number;
+  timestamp: number;
+}
+
+/**
+ * A generic object to buffer the value changes for a minimum duration.
+ *  Example usage:
+ *  ```javascript
+ *  let connectionState = new TimeBuffered({defaultMinDuration:1000, isRemoveLastDuplicated:true});
+ *  connectionState.push("connecting...",500);
+ *  connectionState.push("connected",2000);
+ *  connectionState.push();
+ *  connectionState.push("connection lost");
+ *  connectionState.push("re-connecting...",500);
+ *  connectionState.push("connection failed");
+ *  // get the current values in the buffer
+ *  let currentConnectionStates[] = connectionState.get();
+ *  // the current internal buffered items which have the time details, it returns a new array if there was any new push, so it can be used for listening.
+ *  let timeBufferedItems[] = connectionState.timeBufferedItems;
+ * ```
+ */
+export default class TimeBuffered<T> {
+  public timeBufferedItems = new Array<TimeBufferedItem<T>>();
+  public isRemoveLastDuplicated?: boolean = false;
+  public defaultMinDuration?: number;
+
+  /**
+   * Constuct the value buffer with two optional options:
+   * @param defaultMinDuration optinal,used if no minDuration parameter given on push
+   * @param isRemoveLastDuplicated optinal, if true, will try to remove the last value in the buffer immediately if it DEEP equals with the new value
+   */
+  public TimeBuffered(options?: {
+    defaultMinDuration?: number;
+    isRemoveLastDuplicated?: boolean;
+  }) {
+    this.defaultMinDuration = options?.defaultMinDuration;
+    this.isRemoveLastDuplicated = options?.isRemoveLastDuplicated;
+  }
+
+  get(): T[] {
+    return this.timeBufferedItems.map(item => item.value);
+  }
+
+  public size() {
+    return this.timeBufferedItems.length;
+  }
+
+  public push(value?: T, minDuration?: number) {
+    const now = Date.now();
+    minDuration = minDuration ?? this.defaultMinDuration ?? 0;
+
+    // try remove the last duplicated message if the option enabled
+    let items =
+      value != undefined && this.isRemoveLastDuplicated
+        ? this.removeLastDuplicated(value)
+        : this.timeBufferedItems;
+
+    // push away expried messages
+    items = items.filter(i => i.timestamp > now - i.minDuration);
+
+    //return if no value to save
+    if (value == undefined) {
+      this.timeBufferedItems = items;
+      return;
+    }
+
+    // push the current message with a timestamp
+    const item: TimeBufferedItem<T> = {
+      value,
+      minDuration,
+      timestamp: Date.now()
+    };
+
+    // ask the last old value if any to move away later when the min duration is completed
+    if (items.length > 0) {
+      const last = items[0];
+      const durationLeft = item.minDuration - (now - last.timestamp);
+      setTimeout(
+        () => {
+          this.remove(last.value);
+        },
+        durationLeft < 0 ? 0 : durationLeft
+      );
+    }
+
+    this.timeBufferedItems = [item, ...items];
+  }
+
+  private removeLastDuplicated(value: T) {
+    if (
+      this.timeBufferedItems.length > 0 &&
+      deepEqual(this.timeBufferedItems[0].value, value)
+    ) {
+      return this.timeBufferedItems.slice(1);
+    } else {
+      return this.timeBufferedItems;
+    }
+  }
+
+  private remove(value: T): void {
+    console.info("remove :", value);
+    const index = this.timeBufferedItems.findIndex(s =>
+      deepEqual(s.value, value)
+    );
+    if (index >= 0) {
+      this.timeBufferedItems = [
+        ...this.timeBufferedItems.slice(0, index),
+        ...this.timeBufferedItems.slice(index + 1)
+      ];
+    }
+  }
+}
+
+function deepEqual(x: any, y: any) {
+  if (x === y) {
+    return true;
+  } else if (
+    typeof x === "object" &&
+    x !== null &&
+    typeof y === "object" &&
+    y !== null
+  ) {
+    if (Object.keys(x).length !== Object.keys(y).length) return false;
+
+    for (const prop in x) {
+      if (y.hasOwnProperty(prop)) {
+        if (!deepEqual(x[prop], y[prop])) return false;
+      } else return false;
+    }
+
+    return true;
+  } else return false;
+}
